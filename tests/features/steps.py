502a0fe1745b631f361af10e9fa1ea3753c25795
Luis Angel Sustaita <sustaita_7_7@hotmail.com>
2014-09-16 22:29:55 -0500

    Signed-off-by: Luis Angel Sustaita <sustaita_7_7@hotmail.com>

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -2,34 +2,29 @@
 from lettuce import *
 import app.match as m
 
-
 @step(u'Given: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def given_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    world.match = m.Match(group1,group2,group3)
-
-
-@step(u'When: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def when_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    assert False, 'This step must be implemented'
+def given_players_and_player2_start_match_to_num_sets(step, player1, player2, sets):
+	world.match = m.Match(player1, player2, int(sets))
 
 
 @step(u'Then: I see score: "([^"]*)"')
-def then_i_see_score_group1(step, group1):
-    assert False, 'This step must be implemented'
+def then_i_see_score_group1(step, score):
+	assert world.match.score() == score, \
+		"Got %s " % world.match.score()
 
 
 @step(u'When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
-def when_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+def when_player_won_the_numberOfSet_set_p1_p2(step, player, set_number, p1, p2):
+    world.match.win_set(player, int(set_number[0]), p1, p2)
+    
 
 
 @step(u'And: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
-def and_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
-
+def and_player_won_the_numberOfSet_set_p1_p2(step, player, set_number, p1, p2):
+	world.match.win_set(player, int(set_number[0]), p1, p2)
+    
 
 @step(u'Then: The match score is: "([^"]*)"')
-def then_the_match_score_is_group1(step, group1):
-    assert False, 'This step must be implemented'
-
-
+def then_the_match_score_is_group1(step, score):
+	assert world.match.score() == score, \
+		"Got %s " % world.match.score()
