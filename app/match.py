2f161aae8be51d8429b9d9a2c8e16197870046e0
Luis Angel Sustaita <sustaita_7_7@hotmail.com>
2014-09-16 23:11:57 -0500

    Signed-off-by: Luis Angel Sustaita <sustaita_7_7@hotmail.com>

--- app/match.py
+++ app/match.py
@@ -4,3 +4,7 @@ class Match:
         self.p1 = player1
         self.p2 = player2
         self.pacted_sets = pacted_sets
+        self.p1_wins = 0
+        self.p2_wins = 0
+        self.p1_sets = ['', '', '', '', '']
+        self.p2_sets = ['', '', '', '', '']
